ff3ba41c04c565d4d1ae14f395ea63228faf37b6
Sergiy Sych <sychsergiy@gmail.com>
2018-05-21 12:30:53 +0300

    Object socialapp_id fields added and relationship

--- app/tables/objects.py
+++ app/tables/objects.py
@@ -1,6 +1,7 @@
 from sqlalchemy import (
     Column, String, Integer, ForeignKey, Text
 )
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
@@ -8,15 +9,18 @@ from app.database import Base
 class Object(Base):
     __tablename__ = 'object'
     id = Column(Integer, primary_key=True)
-
     title = Column(String(255), nullable=False)
     description = Column(Text)
+
+    socialapp_id = Column(Integer, ForeignKey('socialapp.id'), nullable=False)
+    socialapp = relationship('SocialApp')
 
 
 class Operation(Base):
     __tablename__ = 'operation'
     id = Column(Integer, primary_key=True)
-
-    object_id = Column(ForeignKey('object.id'), nullable=False)
     title = Column(String(255), nullable=False)
     description = Column(Text)
+
+    object_id = Column(ForeignKey('object.id'), nullable=False)
+    object = relationship('Object')
