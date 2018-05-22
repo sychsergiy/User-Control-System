feadfdd2f0009df653fcfe502d85f423039e56b1
Sergiy Sych <sychsergiy@gmail.com>
2018-05-22 09:58:53 +0300

    get database config from environment

--- alembic/env.py
+++ alembic/env.py
@@ -1,13 +1,15 @@
 from __future__ import with_statement
 from alembic import context
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import create_engine
 from logging.config import fileConfig
 
 import sys
-import os.path
+import os
+
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from app.database import get_metadata
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -21,6 +23,7 @@ fileConfig(config.config_file_name)
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = get_metadata()
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -40,12 +43,20 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url, target_metadata=target_metadata, literal_binds=True)
 
     with context.begin_transaction():
         context.run_migrations()
+
+
+def get_url():
+    try:
+        url = os.environ['DATABASE_URL']
+    except KeyError:
+        raise EnvironmentError('Provide DATABASE_URL environment variable')
+    return url
 
 
 def run_migrations_online():
@@ -55,11 +66,7 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix='sqlalchemy.',
-        poolclass=pool.NullPool)
-
+    connectable = create_engine(get_url())
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
@@ -68,6 +75,7 @@ def run_migrations_online():
 
         with context.begin_transaction():
             context.run_migrations()
+
 
 if context.is_offline_mode():
     run_migrations_offline()
--- app/database.py
+++ app/database.py
@@ -1,10 +1,21 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine('mysql://ucs_admin:adminpass@localhost/user_control_system')
-Base = declarative_base()
 
 
 def get_metadata():
     from app import tables
     return Base.metadata
+
+
+def get_database_url():
+    try:
+        create_engine(os.environ['DATABASE_URL'])
+    except KeyError:
+        raise EnvironmentError('Provide DATABASE_URL environment variable')
+
+
+engine = get_database_url()
+
+Base = declarative_base()
